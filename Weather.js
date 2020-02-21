c612ebfd9aeefedc6d39cbb9348521fb9ccb871c
HiHoi <ghtjd5087@gmail.com>
2020-02-21 16:37:15 +0900

    Change subtitle & build apk

--- Weather.js
+++ Weather.js
@@ -8,26 +8,26 @@ const weatherOptions = {
   Thunderstorm: {
     iconName: "weather-lightning",
     gradient: ["#373B44", "#4286f4"],
-    title: "Thunderstorm",
-    subtitle: "Actually, outside of the house"
+    title: "폭풍",
+    subtitle: "이럴 때 집에 있는게 낫지~"
   },
   Drizzle: {
     iconName: "weather-hail",
     gradient: ["#89F7FE", "#66A6FF"],
-    title: "Drizzle",
-    subtitle: "Is like rain"
+    title: "이슬비",
+    subtitle: "기분이 나쁜 비"
   },
   Rain: {
     iconName: "weather-rainy",
     gradient: ["#00C6FB", "#005BEA"],
-    title: "Raining like a MF",
-    subtitle: "For more info look outside"
+    title: "비",
+    subtitle: "우산 꼭 챙기기"
   },
   Snow: {
     iconName: "weather-snowy",
     gradient: ["#7DE2FC", "#B9B6E5"],
-    title: "Cold as balls",
-    subtitle: "Do you want to build a snowman? Fuck no."
+    title: "눈",
+    subtitle: "나랑 눈사람 만들래~"
   },
   Atmosphere: {
     iconName: "weather-hail",
@@ -36,32 +36,32 @@ const weatherOptions = {
   Clear: {
     iconName: "weather-sunny",
     gradient: ["#FF7300", "#FEF253"],
-    title: "Sunny as fuck",
-    subtitle: "Go get your ass burnt"
+    title: "맑음",
+    subtitle: "날씨 좋으면 꼭 약속이 없더라..."
   },
   Clouds: {
     iconName: "weather-cloudy",
     gradient: ["#D7D2CC", "#304352"],
-    title: "Clouds",
-    subtitle: "I know, fucking boring"
+    title: "구름",
+    subtitle: "어둑어둑한게 비오겠네"
   },
   Mist: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
-    title: "Mist!",
-    subtitle: "It's like you have no glasses on."
+    title: "옅은 안개",
+    subtitle: "미세먼지는 아니니 걱정 NO!"
   },
   Dust: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
-    title: "Dusty",
-    subtitle: "Thanks a lot China "
+    title: "미세먼지",
+    subtitle: "비염환자는 마스크 챙기기"
   },
   Haze: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
-    title: "Haze",
-    subtitle: "Just don't go outside."
+    title: "짙은 안개",
+    subtitle: "구름 안에 있는거 같애.."
   }
 };
 
